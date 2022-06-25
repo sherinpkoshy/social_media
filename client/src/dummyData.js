5ae10653bb0d3f17052ff96f2ba8273f7b16d5fb
sherin <sherinpkoshy07@gmail.com>
2022-06-25 12:14:46 +0530

    comit

---
+++ client/src/dummyData.js
@@ -0,0 +1,75 @@
+export const Users = [
+  {
+    id: 1,
+    profilePicture: "person/1.jpeg",
+    username: "sachin",
+  },
+  {
+    id: 2,
+    profilePicture: "person/2.jpeg",
+    username: "cathe",
+  },
+  {
+    id: 3,
+    profilePicture: "person/3.jpeg",
+    username: "rose",
+  },
+  {
+    id: 4,
+    profilePicture: "person/4.jpeg",
+    username: "jhuan",
+  },
+  {
+    id: 5,
+    profilePicture: "person/5.jpeg",
+    username: "mike",
+  },
+];
+
+export const Posts = [
+  {
+    id: 1,
+    desc: "Love for all",
+    photo: "posts/1.jpeg",
+    date:"5 min ago",
+    userId:1,
+    like:32,
+    comment:14
+  },
+  {
+    id: 2,
+    desc: "Love for all",
+    photo: "/posts/2.jpeg",
+    date:"10 min ago",
+    userId:2,
+    like:50,
+    comment:27
+  },
+  {
+    id: 3,
+    desc: "Love for all",
+    photo: "/posts/3.jpeg",
+    date:"1 hr ago",
+    userId:3,
+    like:12,
+    comment:4
+  },
+  {
+    id: 4,
+    desc: "Love for all",
+    photo: "/posts/4.jpeg",
+    date:"4 hrs ago",
+    userId:4,
+    like:3,
+    comment:1
+  },
+  {
+    id: 5,
+    desc: "Love for all",
+    photo: "/posts/5.jpeg",
+    date:"12 hrs ago",
+    userId:5,
+    like:32,
+    comment:9
+  },
+];
